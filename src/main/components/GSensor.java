aebfa128e879b49498d0a6310098cfc295b6d69f
Alan <112457025+Cortes205@users.noreply.github.com>
2022-11-16 23:55:13 -0500

    Update GSensor.java

--- src/main/components/GSensor.java
+++ src/main/components/GSensor.java
@@ -13,7 +13,11 @@ public class GSensor extends EV3GyroSensor
 		this.sample = new float[this.sampleSize()];
 	}
 	
-	public boolean veeringLeft()
+	/* This allows the robot to detect itself in a 10 degree lane. if it is veering left
+	* it will return true on an angle less than -5, and if it veering right it will
+	* return true on an angle greater than 5. tbh idek if this works but it seems
+	* that the robot reacts in some way */
+	public boolean veeringLeft() 
 	{
 		this.getAngleMode().fetchSample(sample, 0);
 		return sample[0] < -5;
